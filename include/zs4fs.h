c2d231f353121fe081e908c704855e80bea88283
count inn <countinn@gmail.com>
2015-04-15 16:33:17 -0300

    Portable directory list

--- include/zs4fs.h
+++ include/zs4fs.h
@@ -150,85 +150,8 @@ public:
 	static const bool TAB_FS_IS_INSENSITIVE;
 	static const bool TAB_FS_HAS_BACKSLASHES;
 
-	inline size_t List(const char * name, bool hidden_files){
+	size_t List(const char * name, bool hidden_files);
 
-			count = 0;
-
-			if (IsFile(name))
-				return 0;
-
-			if (!IsDir(name))
-				return 0;
-
-			zs4stat * nu;
-
-#ifdef _WIN32
-			WIN32_FIND_DATA data;
-
-			zs4StringBufferWide win_name;
-
-			if (!MultiByteToWideChar(CP_OEMCP, 0, name, -1, (LPWSTR)win_name.str, ZS4_STRINGBUFFER_SIZE))
-				return 0;
-
-			HANDLE h = FindFirstFile(win_name.str, &data);
-			if ( h == INVALID_HANDLE_VALUE)
-				return 0;
-
-			zs4StringBuffer byte;
-
-			WideCharToMultiByte(CP_OEMCP, 0, (LPCWCH)data.cFileName, -1, byte.str, ZS4_STRINGBUFFER_SIZE, NULL, NULL);
-
-			if ((NULL == (nu = nuStat())) )
-			{
-				FindClose(h);
-				return count;
-			}
-			else
-			{
-				while (FindNextFile(h, &data))
-				{
-					WideCharToMultiByte(CP_OEMCP, 0, (LPCWCH)data.cFileName, -1, byte.str, ZS4_STRINGBUFFER_SIZE, NULL, NULL);
-
-					if ((NULL == (nu = nuStat())) || zs4SUCCESS != nu->GetInfo(byte.str))
-					{
-						FindClose(h);
-						return count;
-					}
-				}
-			}
-			FindClose(h);
-
-#else
-
-		DIR * dir = opendir(name);
-		if (dir != NULL)
-		{
-			struct dirent * de = 0;
-
-			while (NULL!=(de = readdir(dir)))
-			{
-				if ((de->d_name && de->d_name[0])
-					&& (strcmp(de->d_name, "."))
-					&& (strcmp(de->d_name, ".."))
-					)
-				{
-					if (!hidden_files && de->d_name[0] == '.')
-						continue;
-
-					if ((NULL == (nu = nuStat())) || zs4SUCCESS != nu->GetInfo(de->d_name))
-					{
-						closedir(dir);
-						return count;
-					}
-				}
-			}
-
-			closedir(dir);
-		}
-
-#endif
-		return count;
-	}
 	inline zs4stat * nuStat(void)	{
 		if (count >= (ZS4_MAX_DIR_SIZE - 1))
 			return NULL;
