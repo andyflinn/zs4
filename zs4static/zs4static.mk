ba8fe75a74482e20b836992b60fe19bf88d5285c
countlinux <countinn@gmail.com>
2015-05-09 13:55:39 -0300

    suse

--- zs4static/zs4static.mk
+++ zs4static/zs4static.mk
@@ -58,7 +58,7 @@ CFLAGS   :=  -g $(Preprocessors)
 ## User defined environment variables
 ##
 CodeLiteDir:=/usr/share/codelite
-Objects0=$(IntermediateDirectory)/zs4ssl$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/zs4crypt$(ObjectSuffix) $(IntermediateDirectory)/zs4fs$(ObjectSuffix) 
 
 
 
@@ -87,13 +87,21 @@ PreBuild:
 ##
 ## Objects
 ##
-$(IntermediateDirectory)/zs4ssl$(ObjectSuffix): zs4ssl.cpp $(IntermediateDirectory)/zs4ssl$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/countinn/zs4/zs4static/zs4ssl.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/zs4ssl$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/zs4ssl$(DependSuffix): zs4ssl.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/zs4ssl$(ObjectSuffix) -MF$(IntermediateDirectory)/zs4ssl$(DependSuffix) -MM "zs4ssl.cpp"
+$(IntermediateDirectory)/zs4crypt$(ObjectSuffix): zs4crypt.cpp $(IntermediateDirectory)/zs4crypt$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/countinn/zs4/zs4static/zs4crypt.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/zs4crypt$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/zs4crypt$(DependSuffix): zs4crypt.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/zs4crypt$(ObjectSuffix) -MF$(IntermediateDirectory)/zs4crypt$(DependSuffix) -MM "zs4crypt.cpp"
 
-$(IntermediateDirectory)/zs4ssl$(PreprocessSuffix): zs4ssl.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/zs4ssl$(PreprocessSuffix) "zs4ssl.cpp"
+$(IntermediateDirectory)/zs4crypt$(PreprocessSuffix): zs4crypt.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/zs4crypt$(PreprocessSuffix) "zs4crypt.cpp"
+
+$(IntermediateDirectory)/zs4fs$(ObjectSuffix): zs4fs.cpp $(IntermediateDirectory)/zs4fs$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/countinn/zs4/zs4static/zs4fs.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/zs4fs$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/zs4fs$(DependSuffix): zs4fs.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/zs4fs$(ObjectSuffix) -MF$(IntermediateDirectory)/zs4fs$(DependSuffix) -MM "zs4fs.cpp"
+
+$(IntermediateDirectory)/zs4fs$(PreprocessSuffix): zs4fs.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/zs4fs$(PreprocessSuffix) "zs4fs.cpp"
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
@@ -101,9 +109,12 @@ $(IntermediateDirectory)/zs4ssl$(PreprocessSuffix): zs4ssl.cpp
 ## Clean
 ##
 clean:
-	$(RM) $(IntermediateDirectory)/zs4ssl$(ObjectSuffix)
-	$(RM) $(IntermediateDirectory)/zs4ssl$(DependSuffix)
-	$(RM) $(IntermediateDirectory)/zs4ssl$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/zs4crypt$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/zs4crypt$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/zs4crypt$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/zs4fs$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/zs4fs$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/zs4fs$(PreprocessSuffix)
 	$(RM) $(OutputFile)
 	$(RM) "../.build-debug/zs4static"
 
